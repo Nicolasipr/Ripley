d2ec4115dae407802091b3c7adee5a27fd7c3787
Nicolas Perez Roa <nicolasipr.97@gmail.com>
2020-01-24 04:04:57 -0300

    adding menu options

--- 2.- Linked List/main.cpp
+++ 2.- Linked List/main.cpp
@@ -1,4 +1,26 @@
 //
 // Created by nicolasipr on 24/01/2020.
 //
+#include <iostream>
 
+#include "Node.h"
+#include "LinkedList.h"
+
+using namespace std;
+
+int main() {
+
+    LinkedList Simple;
+
+    int N; cin >> N;
+    for (int i = 0; i <= N; i++) {
+        Simple.addNode(i);
+    }
+    Simple.displayList();
+
+    int k; cin >> k;
+    Simple.findAndDelete(k);
+
+    Simple.displayList();
+    return 0;
+}
