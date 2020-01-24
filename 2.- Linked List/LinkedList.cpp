78c404a0fceb93e8310c7164f38212b37b5a1692
Nicolas Perez Roa <nicolasipr.97@gmail.com>
2020-01-24 02:13:47 -0300

    setting up methods

--- 2.- Linked List/LinkedList.cpp
+++ 2.- Linked List/LinkedList.cpp
@@ -1,5 +1,48 @@
 //
 // Created by nicolasipr on 24/01/2020.
 //
-
+#include <iostream>
 #include "LinkedList.h"
+
+using namespace std;
+
+LinkedList::LinkedList() {
+    // constructor
+}
+LinkedList::~LinkedList() {
+    // destructor
+}
+
+void LinkedList::addNode(int n_data) {
+    Node *node =  new Node();
+    node->data = n_data;
+    node->next = NULL;
+
+    if( head ){
+        head = node;
+    }
+    else{
+        Node *aux = head;
+        while ( aux->next ){
+            aux = aux->next;
+        }
+        aux->next = node;
+    }
+
+}
+int LinkedList::find(int k) {
+
+    if( head ){
+        cout << "List is empty! " << endl;
+    }
+    else{
+
+    }
+
+}
+void LinkedList::findAndDelete(int k) {
+
+}
+void LinkedList::remove(int k){
+
+}
--- 2.- Linked List/LinkedList.h
+++ 2.- Linked List/LinkedList.h
@@ -5,12 +5,21 @@
 #ifndef RIPLEY_LINKEDLIST_H
 #define RIPLEY_LINKEDLIST_H
 
-
+#include "Node.h"
 
 class LinkedList {
+private:
+    Node *head;
+
+public:
+    LinkedList();
+    ~LinkedList();
+    void addNode(int n_data);
+    int find(int k);
+    void remove(int k);
+    void findAndDelete(int k);
 
 };
-
 
 
 #endif //RIPLEY_LINKEDLIST_H
