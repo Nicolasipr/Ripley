97bdbba0bd646b613c14f708c6754c057cde477d
Nicolas Perez Roa <nicolasipr.97@gmail.com>
2020-01-27 02:14:24 -0300

    Solving 3.A
    Using self implemented BFS to solve it

--- 3.- Graph & Trees/graph.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by nicolasipr on 24/01/2020.
-//
-
-#ifndef RIPLEY_GRAPH_H
-#define RIPLEY_GRAPH_H
-
-#endif //RIPLEY_GRAPH_H
--- 3.- Graph & Trees/main.cpp
+++ 3.- Graph & Trees/main.cpp
@@ -2,3 +2,89 @@
 // Created by nicolasipr on 24/01/2020.
 //
 
+#include <iostream>
+#include <bitset>
+#include <queue>
+
+#define MAX 128
+
+using namespace std;
+
+int graph[4][4] = {{0, 1, 0, 0},
+                   {0, 0, 1, 0},
+                   {1, 0, 0, 0},
+                   {0, 0, 1, 0}};
+
+
+
+bool BFS(int A, int B){
+
+    if(A==B) // if both nodes are equal, they are already connected
+        return true;
+
+    bitset<MAX> visited;
+    queue<int> path;
+
+    path.push(A); // add A node to path
+    visited[A] = 1; // and marked as visited
+
+    while( !path.empty() ){
+
+        int i = path.front(); // starts with the first adjacent node on the queue
+        path.pop(); // once it has been setted,
+
+        for(int j = 0; j < 4; j++){
+
+            if( graph[i][j] ){ // if has an adjacent node
+
+                if( !visited[j] ){ // if not visited yet
+                    if(j == B){ // When found node
+                        return true;
+                    }
+                    else{
+                        visited[j] = 1;
+                        path.push(j);
+                    }
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+
+int main() {
+
+
+
+    cout << "\n\n\t\tGraph\n" << endl;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int A;
+    cout << "\n\nInput A Node: " ;
+    cin >> A;
+
+    int B;
+    cout << "Input B Node: " ;
+    cin >> B;
+
+    cout << "Are Node \"" << A << "\" and node \"" << B << "\" connected? " << endl;
+    if( BFS(A,B) )
+        cout << "R: Yes" << endl;
+    else
+        cout << "R: No" << endl;
+
+    cout << "Are Node \"" << B << "\" and node \"" << A << "\" connected? " << endl;
+    if( BFS(B,A) )
+        cout << "R: Yes" << endl;
+    else
+        cout << "R: No" << endl;
+
+    return 0;
+}
